65019f50ac5c022c98e428ac394bbba9b75dbbb1
Hwang Sangmin <hwsmin920@gmail.com>
2024-02-16 21:49:04 +0900

    feat: Return a row as dictionary

--- db/models/base.py
+++ db/models/base.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
-    pass
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}

e8914eec393943b5b544e3a816799dbe6550fbef
Hwang Sangmin <hwsmin920@gmail.com>
2024-02-28 17:09:32 +0900

    feat: Generate Embedding

---
+++ recsys/embed_generator.py
@@ -0,0 +1,9 @@
+from sentence_transformers import SentenceTransformer
+from chromadb import Documents, EmbeddingFunction, Embeddings
+
+
+class EmbedGenerator(EmbeddingFunction):
+    MODEL_NAME = 'jhgan/ko-sroberta-multitask'
+    model = SentenceTransformer(MODEL_NAME)
+    def __call__(self, input: Documents) -> Embeddings:
+        return self.model.encode(input).tolist()
